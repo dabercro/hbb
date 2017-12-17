2506d4794ae562f34d8228f6c2b2886f701c52dd
dabercro <daniel.r.abercrombie@gmail.com>
2017-12-17 12:50:36 -0500

    Still need lepton ID correct.

--- slimmer/CrombieSlimmingConfig.sh
+++ slimmer/CrombieSlimmingConfig.sh
@@ -4,7 +4,7 @@ export CrombieNLocalProcs=6
 export CrombieExe=makehbbtree
 export CrombieInSample=pandaf/008
 export CrombieOutDir=/data/t3home000/dabercro/fullhbb/$(date +%y%m%d)
-export CrombieFilesPerJob=25
+export CrombieFilesPerJob=15
 
 #export CrombieFullDir=/local/dabercro/pandaexpr/v_008_v2
 export CrombieFullDir=/mnt/hadoop/scratch/dhsu/dylansVHSkims
