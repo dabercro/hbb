b8ac9f60e69b6508d6b1c95a915417e96d2f3542
dabercro <dabercro@mit.edu>
2018-03-27 23:19:52 -0400

    Draft of slides.

--- docs/180328/figs/download.sh
+++ docs/180328/figs/download.sh
@@ -26,10 +26,21 @@ mv overtrain*.png over.png
 
 cd -
 
+test -d bm || mkdir bm
+cd bm
+
+wget http://t3serv001.mit.edu/~bmaier/figs/hbb/zhnn/v7/resolved/cr_ttbar/cr_ttbar_jetCMVA.pdf
+wget http://t3serv001.mit.edu/~bmaier/figs/hbb/zhnn/v7/resolved/cr_ttbar/cr_ttbar_CMVA2_postfit.pdf
+wget http://t3serv001.mit.edu/~bmaier/figs/hbb/zhnn/v7/resolved/sr/sr_BDT.pdf
+
+cd -
+
 crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=180209_v1%2Fregression_withgen%2C180209_v1%2Fregression"
+
 crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=180327_v1%2Finclusive_signal_event_class_reg_3%2C180327_yesterday_post%2Finclusive_signal_event_class_reg_3"
 crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=180327_inc%2Finclusive_tt_cmva_jet2_cmva%2C180327_post%2Finclusive_tt_cmva_jet2_cmva%2C180327_inc%2Finclusive_lightz_cmva_jet2_cmva%2C180327_post%2Finclusive_lightz_cmva_jet2_cmva%2C180327_inc%2Finclusive_heavyz_cmva_jet2_cmva%2C180327_post%2Finclusive_heavyz_cmva_jet2_cmva"
 crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=180326_v3%2Fboosted_signal_fatjet1_mSD_corr%2C180327_yesterday_post%2Fboosted_signal_fatjet1_mSD_corr%2C180326_v3%2Fboosted_tt_fatjet1_mSD_corr%2C180327_yesterday_post%2Fboosted_tt_fatjet1_mSD_corr%2C180326_v3%2Fboosted_lightz_fatjet1_mSD_corr%2C180327_yesterday_post%2Fboosted_lightz_fatjet1_mSD_corr%2C180326_v3%2Fboosted_heavyz_fatjet1_mSD_corr%2C180327_yesterday_post%2Fboosted_heavyz_fatjet1_mSD_corr"
+crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=180327_v4%2Fboosted_signal_fatjet1_mSD_corr%2C180327_v4%2Fboosted_tt_fatjet1_mSD_corr%2C180327_v4%2Fboosted_lightz_fatjet1_mSD_corr%2C180327_v4%2Fboosted_heavyz_fatjet1_mSD_corr"
 
 for f in 1*/*.pdf
 do
