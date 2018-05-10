41760359bd0e30f10050f3b1d66655e15d2886dd
dabercro <daniel.r.abercrombie@gmail.com>
2018-05-10 13:47:00 -0400

    Added ptD.

--- slimmer/include/regression.h
+++ slimmer/include/regression.h
@@ -3,17 +3,15 @@
 namespace regression {
 
   // Get the directions of the PUPPI particles in the area
-  class PuppiInfo {
+  class JetInfo {
   public:
     using type = TLorentzVector;
     using scalar = float;
-    // Charged part of jet
+
+    // PUPPI vectors
     type charged {};
-    // Neutral part of jet
     type neutral {};
-    // Charged pileup
     type charged_pu {};
-    // Neutral pileup
     type neutral_pu {};
 
     // Rings each are separate vector element
@@ -22,13 +20,23 @@ namespace regression {
     rings neut_rings = rings(5);
     rings ch_rings = rings(5);
     rings mu_rings = rings(5);
+
+    // Additional information about the vertex and pf candidates
+    scalar ptD {0};
   };
 
-  PuppiInfo GetPuppiInfo(const panda::Jet& jet) {
-    PuppiInfo output {};
+  JetInfo GetJetInfo(const panda::Jet& jet) {
+    JetInfo output {};
 
+    // For the ptD calculation
+    float sum_weight {0};
+    float sum_pt {0};
+
+    // Fill information about the constituents
     for (auto pf : jet.constituents) {
       auto p4 = pf->p4();
+
+      // PUPPI
       auto puppi = pf->puppiW();
       if (pf->q()) {
         output.charged += p4 * puppi;
@@ -39,30 +47,35 @@ namespace regression {
         output.neutral_pu += p4 * (1.0 - puppi);
       }
 
-      // Get the index of the ring to add the energy to
+      // Energy Rings
       static const std::vector<double> dr_bins {
         std::pow(0.05, 2), std::pow(0.1, 2), std::pow(0.2, 2), std::pow(0.3, 2), std::pow(0.4, 2)
       };
       unsigned bin = std::lower_bound(dr_bins.begin(), dr_bins.end(),
                                       deltaR2(p4.Eta(), p4.Phi(), jet.eta(), jet.phi())) - dr_bins.begin();
-
       if (bin < dr_bins.size()) {
         // Get which ring offset
         auto rings = [&pf] () {
           unsigned pdgid = std::abs(pf->pdgId());
           if (pdgid == 22 || pdgid == 11)
-            return &PuppiInfo::em_rings;
+            return &JetInfo::em_rings;
           else if (pdgid == 13)
-            return &PuppiInfo::mu_rings;
+            return &JetInfo::mu_rings;
           else if (pf->q())
-            return &PuppiInfo::ch_rings;
-          return &PuppiInfo::neut_rings;
+            return &JetInfo::ch_rings;
+          return &JetInfo::neut_rings;
         } ();
         // Apply info
         (output.*rings)[bin].first += p4.E();
         (output.*rings)[bin].second += p4;
       }
+
+      // ptD
+      sum_weight += std::pow(p4.Pt(), 2);
+      sum_pt += p4.Pt();
     }
+
+    output.ptD = sum_weight/sum_pt;
 
     return output;
   }
