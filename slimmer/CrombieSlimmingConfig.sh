912a24d95827ffa2eb5fd86ddae0a543689f385b
Daniel Abercrombie <dabercro@mit.edu>
2019-08-16 14:01:17 -0400

    2016 MC

--- slimmer/CrombieSlimmingConfig.sh
+++ slimmer/CrombieSlimmingConfig.sh
@@ -1,7 +1,7 @@
 #inp_dir=/mnt/hadoop/scratch/dabercro/hbb
 inp_dir=/data/t3home000/dabercro/hbb
-version='014'
-fin_dir=190815_data_$version
+version='009'
+fin_dir=190816_mc2016_$version
 
 # Condor
 #export CrombieExe='eval input_dir=$INPUT_DIR regresstree'
