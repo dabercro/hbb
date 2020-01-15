5e99c1e4b39ee63fa229f12b140834aaed2553d7
Daniel Abercrombie <dabercro@mit.edu>
2020-01-15 16:48:51 -0500

    Starting plots

--- analysis/roofit/addxs.py
+++ analysis/roofit/addxs.py
@@ -5,6 +5,16 @@ import sys
 from CrombieTools.AnalysisTools.XSecAdder import RunXSecAdder, xSecAdder
 from CrombieTools.CommonTools.FileConfigReader import SetupConfigFromEnv, SetFunctionFromEnv
 
+#    <weight id="1" MUR="1" MUF="1" PDF="306000"> mur=1 muf=1 </weight>
+#    <weight id="2" MUR="1" MUF="2" PDF="306000"> mur=1 muf=2 </weight>
+#    <weight id="3" MUR="1" MUF="0.5" PDF="306000"> mur=1 muf=0.5 </weight>
+#    <weight id="4" MUR="2" MUF="1" PDF="306000"> mur=2 muf=1 </weight>
+#    <weight id="5" MUR="2" MUF="2" PDF="306000"> mur=2 muf=2 </weight>
+#    <weight id="6" MUR="2" MUF="0.5" PDF="306000"> mur=2 muf=0.5 </weight>
+#    <weight id="7" MUR="0.5" MUF="1" PDF="306000"> mur=0.5 muf=1 </weight>
+#    <weight id="8" MUR="0.5" MUF="2" PDF="306000"> mur=0.5 muf=2 </weight>
+#    <weight id="9" MUR="0.5" MUF="0.5" PDF="306000"> mur=0.5 muf=0.5 </weight>
+
 if __name__ == '__main__':
 
     xSecAdder.AddMergeBranch('genWeight')
--- docs/200115/figs/download.sh
+++ docs/200115/figs/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=200115_1D_bins%2Fsmearplot_1_jet1_response%2C200115_1D_bins%2Fsmearplot_2_jet1_response%2C200115_1D_bins%2Fsmearplot_3_jet1_response%2C200115_1D_bins%2Fsmearplot_4_jet1_response"
 
 for f in */*.pdf
 do
