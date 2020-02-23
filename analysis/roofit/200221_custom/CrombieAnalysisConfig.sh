638f5c0226a79e0e8d9dcede2b409364369ac65e
Daniel Abercrombie <dabercro@mit.edu>
2020-02-22 22:28:58 -0500

    Add old RooFit files

---
+++ analysis/roofit/200221_custom/CrombieAnalysisConfig.sh
@@ -0,0 +1,4 @@
+#export CrombieMCConfig=MCConfig.txt
+export CrombieMCConfig=CustomConfig.txt
+export CrombieInFilesDir=/data/t3home000/dabercro/nano/smearnano/200221_smearnano_2018_custom
+export CrombieNLocalProcs=6
