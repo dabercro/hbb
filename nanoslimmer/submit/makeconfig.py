665c1cdb4537576117ad65eed3eac0cb95e5f589
Daniel Abercrombie <dabercro@mit.edu>
2020-03-03 15:50:49 -0500

    submit smear

--- nanoslimmer/submit/makeconfig.py
+++ nanoslimmer/submit/makeconfig.py
@@ -8,9 +8,9 @@ import shutil
 
 year='2018'
 
-#exe='smearnano'
-exe='hbbnano'
-version='200223_%s_%s_fin' % (exe, year)
+exe='smearnano'
+#exe='hbbnano'
+version='200303_%s_%s' % (exe, year)
 
 use_custom = True
 files_per_job = 5
