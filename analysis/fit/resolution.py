982b24dba98edca560353402b76dec468154eab9
Daniel Abercrombie <dabercro@mit.edu>
2020-04-07 14:51:31 -0400

    Add smearing to alpha and adjusted response

--- analysis/fit/resolution.py
+++ analysis/fit/resolution.py
@@ -9,8 +9,8 @@ import numpy
 import random
 
 
-date = '200401'
-end = '2016'
+date = '200331'
+end = '2018_custom'
 
 divbymean = True
 
@@ -279,7 +279,7 @@ for bintype in ['smear', 'rho']:
                 index += 1
 
             for data, mc, gen, name, mini, maxi, fit, makesub in [
-                (data_graph_res, mc_graph_res, gen_graph_res, '#sigma', 0.0, 0.4,
+                (data_graph_res, mc_graph_res, gen_graph_res, '#sigma/#mu', 0.0, 0.4,
                  lambda: ROOT.TF1('resolution', 'TMath::Sqrt(pow([0] * x, 2) + pow([1] * (1 + [2] * x), 2))', 0, 0.3), True),
                 (data_graph_mean, mc_graph_mean, None, '#mu', 0.7, 1.0,
                  lambda: ROOT.TF1('mean', '[0] * x + [1]', 0, 0.3), False)
