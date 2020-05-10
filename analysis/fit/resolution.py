072d22b5bde167c4805de48103184a88558efb56
Daniel Abercrombie <dabercro@mit.edu>
2020-05-10 18:20:35 -0400

    Turn off v7 separation

--- analysis/fit/resolution.py
+++ analysis/fit/resolution.py
@@ -9,7 +9,7 @@ import numpy
 import random
 
 
-date = '200509'
+date = '200510'
 end = '2018_custom'
 
 divbymean = True
@@ -89,31 +89,16 @@ trainings = [
     ('jet1_adjusted_response_smeared_nominal', 'Adjust For Alpha'),
     ('jet1_adjusted_response_smeared_up', 'Adjust For Alpha'),
     ('jet1_adjusted_response_smeared_down', 'Adjust For Alpha'),
-    ('jet1_response_smear_nominal', 'Smear Nominal'),
-    ('jet1_response_smear_up', 'Smear Up'),
-    ('jet1_response_smear_down', 'Smear Down'),
-    ('jet1_response_scale_nominal', 'Scale Nominal'),
-    ('jet1_response_scale_up', 'Scale Up'),
-    ('jet1_response_scale_down', 'Scale Down'),
-#    ('jet1_response_old_scale_nominal', 'Old Scale Nominal'),
-#    ('jet1_response_old_scale_up', 'Old Scale Up'),
-#    ('jet1_response_old_scale_down', 'Old Scale Down'),
-    ('jet1_response_single_smear_nominal', 'Single Bin Smear Nominal'),
-    ('jet1_response_single_smear_up', 'Single Bin Smear Up'),
-    ('jet1_response_single_smear_down', 'Single Bin Smear Down'),
-    ('jet1_response_single_scale_nominal', 'Single Bin Scale Nominal'),
-    ('jet1_response_single_scale_up', 'Single Bin Scale Up'),
-    ('jet1_response_single_scale_down', 'Single Bin Scale Down'),
-    ('jet1_response_unbinned_smear_nominal', 'Unbinned Smear Nominal'),
-    ('jet1_response_unbinned_smear_up', 'Unbinned Smear Up'),
-    ('jet1_response_unbinned_smear_down', 'Unbinned Smear Down'),
-    ('jet1_response_unbinned_scale_nominal', 'Unbinned Scale Nominal'),
-    ('jet1_response_unbinned_scale_up', 'Unbinned Scale Up'),
-    ('jet1_response_unbinned_scale_down', 'Unbinned Scale Down'),
-    ('jet1_response_unbinned_3d_nominal', 'Unbinned 3D Nominal'),
-    ('jet1_response_unbinned_3d_up', 'Unbinned 3D Up'),
-    ('jet1_response_unbinned_3d_down', 'Unbinned 3D Down'),
-    ][0:5]
+    ('jet1_half_adjusted_response_smeared_nominal', 'Adjust For Alpha'),
+    ('jet1_half_adjusted_response_smeared_up', 'Adjust For Alpha'),
+    ('jet1_half_adjusted_response_smeared_down', 'Adjust For Alpha'),
+    ('jet1_adjusted_response_smeared_scaled_nominal', 'Adjust For Alpha'),
+    ('jet1_adjusted_response_smeared_scaled_up', 'Adjust For Alpha'),
+    ('jet1_adjusted_response_smeared_scaled_down', 'Adjust For Alpha'),
+    ('jet1_half_adjusted_response_smeared_scaled_nominal', 'Adjust For Alpha'),
+    ('jet1_half_adjusted_response_smeared_scaled_up', 'Adjust For Alpha'),
+    ('jet1_half_adjusted_response_smeared_scaled_down', 'Adjust For Alpha'),
+    ]
 
 
 for bintype in ['smear', 'rho']:
--- analysis/roofit/CrombieAnalysisConfig.sh
+++ analysis/roofit/CrombieAnalysisConfig.sh
@@ -2,5 +2,5 @@
 #export CrombieMCConfig=MCConfig17.txt
 #export CrombieMCConfig=MCConfig16.txt
 export CrombieMCConfig=CustomConfig.txt
-export CrombieInFilesDir=/data/t3home000/dabercro/nano/smearnano/200509_smearnano_2018_custom
+export CrombieInFilesDir=/data/t3home000/dabercro/nano/smearnano/200509_v2_smearnano_2018_custom
 export CrombieNLocalProcs=12
