a2bbd2a4f78a10152f12aac9840a478775885b7f
Daniel Abercrombie <dabercro@mit.edu>
2020-06-17 14:27:37 -0400

    Modify packing script to include less junk

--- bin/packxbb.sh
+++ bin/packxbb.sh
@@ -2,6 +2,6 @@
 
 cd $CMSSW_BASE/..
 
-tar --exclude '.git' --exclude 'samples' --directory $CMSSW_BASE -czf xbb_condor.tgz python src
+tar --exclude '.git' --exclude 'samples' --exclude 'logs_*' --exclude '*.tar' --exclude 'tree_*.root' --exclude 'condor_*' --directory $CMSSW_BASE -czf xbb_condor.tgz python src
 
 ls -lh xbb_condor.tgz
