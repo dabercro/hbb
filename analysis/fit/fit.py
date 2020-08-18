62b9d9a4d1f19779a0362e33e33b916c8ac5ac97
Daniel Abercrombie <dabercro@mit.edu>
2020-08-18 14:32:28 -0400

    Add low pT bin for signal fit

--- analysis/fit/fit.py
+++ analysis/fit/fit.py
@@ -14,17 +14,17 @@ from HelperPython import Bukin
 newdir = os.path.join(
     os.environ['HOME'],
     'public_html/plots',
-    '%s_fit_v2' % datetime.date.strftime(datetime.datetime.now(), '%y%m%d')
+    '%s_fit' % datetime.date.strftime(datetime.datetime.now(), '%y%m%d')
 )
 
 if not os.path.exists(newdir):
     os.mkdir(newdir)
 
-indir = '/home/dabercro/public_html/plots/200624_Zll2018_runplot-v10'
+indir = '/home/dabercro/public_html/plots/200818_Zll2018_runplot-v1'
 title = ';m_{jj} [GeV];Events/10 GeV'
 
 for jets in ['__', '_0j__', '_1j__', '_ge1j__', '_ge2j__']:
-    for pt in ['medhigh', 'med', 'high']:
+    for pt in ['low', 'medhigh', 'med', 'high']:
 
         functions = []
 
@@ -79,7 +79,8 @@ for jets in ['__', '_0j__', '_1j__', '_ge1j__', '_ge2j__']:
             print filename, '#sigma/#mu = %f' % (function.GetParameter(2)/function.GetParameter(1))
 
         test = ROOT.TCanvas()
-        legend = ROOT.TLegend(0.15, 0.875, 0.4, 0.675)
+        legend = ROOT.TLegend(0.15, 0.875, 0.4, 0.675) if pt != 'low' else \
+            ROOT.TLegend(0.9, 0.875, 0.65, 0.675)
 
         # If more than two jets, draw without regression first
         if pt == 'high':
@@ -88,12 +89,15 @@ for jets in ['__', '_0j__', '_1j__', '_ge1j__', '_ge2j__']:
         elif jets == '_ge2j__':
             functions[2][1].Draw()
 
+        elif pt == 'low':
+            functions[1][1].Draw()
+
         for idx, stuff in enumerate(functions):
 
             label, function = stuff
 
             function.SetLineColor(idx + 1)
-            if idx or jets == '_ge2j__':
+            if idx or jets == '_ge2j__' or pt == 'low':
                 function.Draw('same')
             else:
                 function.Draw()
