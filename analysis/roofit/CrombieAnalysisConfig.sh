c975401eb2f12a5e622db79a3aec95a005e6b104
Daniel Abercrombie <dabercro@mit.edu>
2020-10-08 06:28:03 -0400

    makesingle.sh needs adjustment, I think

--- analysis/roofit/CrombieAnalysisConfig.sh
+++ analysis/roofit/CrombieAnalysisConfig.sh
@@ -3,5 +3,5 @@ export CrombieMCConfig=MCConfig17.txt
 #export CrombieMCConfig=MCConfig16.txt
 #export CrombieMCConfig=MCConfig16_v4.txt
 #export CrombieMCConfig=CustomConfig.txt
-export CrombieInFilesDir=/data/t3home000/dabercro/nano/smearnano/201004_smearnano_2017_v4
+export CrombieInFilesDir=/data/t3home000/dabercro/nano/smearnano/201007_smearnano_2017_v4
 export CrombieNLocalProcs=6
