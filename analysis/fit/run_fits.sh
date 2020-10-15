f100ca7ed12f46279f4f6483a39eb6467b19d123
Daniel Abercrombie <dabercro@mit.edu>
2020-10-14 22:57:46 -0400

    2016 fits

--- analysis/fit/run_fits.sh
+++ analysis/fit/run_fits.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-for plotdir in 201009_2018ABC_v6 201009_2018D_v6
+for plotdir in 201013_2016_nobsf_tight_loose 201013_2016_medium_loose 201013_2016_tight_loose 201013_2016_tight_medium 201013_2016_tight 201013_2016_v4_medium_loose 201013_2016_v4_tight_loose 201013_2016_v4_tight_medium 201013_2016_v4_tight
 do
 
     ./resolution.py $plotdir > outputs/$plotdir.txt
--- analysis/roofit/CrombieAnalysisConfig.sh
+++ analysis/roofit/CrombieAnalysisConfig.sh
@@ -1,7 +1,7 @@
 #export CrombieMCConfig=MCConfig.txt
-export CrombieMCConfig=MCConfig17.txt
-#export CrombieMCConfig=MCConfig16.txt
+#export CrombieMCConfig=MCConfig17.txt
+export CrombieMCConfig=MCConfig16.txt
 #export CrombieMCConfig=MCConfig16_v4.txt
 #export CrombieMCConfig=CustomConfig.txt
-export CrombieInFilesDir=/data/t3home000/dabercro/nano/smearnano/201007_smearnano_2017_v4
+export CrombieInFilesDir=/data/t3home000/dabercro/nano/smearnano/201010_smearnano_2016
 export CrombieNLocalProcs=6
