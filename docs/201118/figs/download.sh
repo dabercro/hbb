acbd146b5322bf4354e3d531a7fa7275e9590dc2
dabercro <dabercro@mit.edu>
2020-11-16 13:24:15 -0500

    Starting slides

---
+++ docs/201118/figs/download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=201114_STXS_VZ_unblinded_XbbVZ_nobtag_d308a317_0d357869%2Fsummary_stxs%2C201115_STXSfine_400split_Xbb_nobtag_f8140c84_0d357869%2Fsummary_stxs%2C201115_VZ_unblinded_XbbVZ_nobtag_d308a317_0d357869%2Fsummary_stxs%2C201115_inclVZ_unblinded_XbbVZ_d308a317_0d357869%2Fsummary_stxs"
+crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=201115_inclVZ_unblinded_XbbVZ_d308a317_0d357869%2Fscan_nominal_r"
+
+for f in */*.pdf
+do
+    pdfcrop $f $f
+done
