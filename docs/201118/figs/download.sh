16702e3704b93e54172032e9879deedf255f1fe2
Daniel Abercrombie <daniel.r.abercrombie@gmail.com>
2020-11-16 22:41:50 -0500

    More slide stuff

--- docs/201118/figs/download.sh
+++ docs/201118/figs/download.sh
@@ -2,6 +2,8 @@
 
 crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=201114_STXS_VZ_unblinded_XbbVZ_nobtag_d308a317_0d357869%2Fsummary_stxs%2C201115_STXSfine_400split_Xbb_nobtag_f8140c84_0d357869%2Fsummary_stxs%2C201115_VZ_unblinded_XbbVZ_nobtag_d308a317_0d357869%2Fsummary_stxs%2C201115_inclVZ_unblinded_XbbVZ_d308a317_0d357869%2Fsummary_stxs"
 crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=201115_inclVZ_unblinded_XbbVZ_d308a317_0d357869%2Fscan_nominal_r"
+crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=201115_STXSfine_400split_Xbb_nobtag_f8140c84_0d357869%2Flimit"
+crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=201116_VZyear_unblinded_XbbVZ_nobtag_d308a317_0d357869%2Fsummary_stxs"
 
 for f in */*.pdf
 do
