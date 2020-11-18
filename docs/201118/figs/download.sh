da9e97be9c15b7441997cbedcddcf7907eb42ea2
Daniel Abercrombie <daniel.r.abercrombie@gmail.com>
2020-11-18 13:06:33 -0500

    Figures are hidden on my desktop

--- docs/201118/figs/download.sh
+++ docs/201118/figs/download.sh
@@ -5,6 +5,10 @@ crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php
 crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=201115_STXSfine_400split_Xbb_nobtag_f8140c84_0d357869%2Flimit"
 crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=201116_VZyear_unblinded_XbbVZ_nobtag_d308a317_0d357869%2Fsummary_stxs"
 
+crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=201117_VZ_unblinded_XbbVZ_31e4e3fa_0d357869%2Fsummary_stxs"
+crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=201118_STXS_VZ%2Fsummary_stxs%2C201118_VZyear%2Fsummary_stxs%2C201118_inclVZ%2Fsummary_stxs"
+crombie downloadtar "http://t3serv001.mit.edu/~dabercro/plotviewer/returntar.php?only=pdf&files=201118_inclVZ%2Fscan_nominal_r"
+
 for f in */*.pdf
 do
     pdfcrop $f $f
